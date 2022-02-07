4327da08cc1bb368072b22d9e0844bb593a65caf
Dhushyanth MK <dhushyanthmohan.kumar2019@vitstudent.ac.in>
2022-02-08 01:50:38 +0530

    Added image upload feature for both methods

--- steganography-compression/src/App.js
+++ steganography-compression/src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import { React, Component } from "react";
 import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-		<ul>
-			<li><Link to="/method1">Method1</Link></li>
-			<li><Link to="/method2">Method2</Link></li>
-		</ul>
-    </div>
-  );
+class App extends Component {
+	render () {
+		return(
+			<div className="App">
+				<ul>
+					<li><Link to="/method1">Method1</Link></li>
+					<li><Link to="/method2">Method2</Link></li>
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
--- steganography-compression/src/templates/Method1.js
+++ steganography-compression/src/templates/Method1.js
@@ -1,11 +1,38 @@
-import React from "react";
+import { React, Component } from "react";
 
-function Method1() {
-    return (
-        <div>
-            Method1
-        </div>
-      );
+class Method1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: null
+        };
+    
+        this.onImageChange = this.onImageChange.bind(this);
+    }
+  
+    onImageChange = event => {
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0];
+            this.setState({
+                image: URL.createObjectURL(img)
+            });
+        }
+    };
+  
+    render() {
+        return (
+            <div>
+                <h1>Method1</h1>
+                <div>
+                    <div>
+                    <img src={this.state.image} />
+                    <h2>Select Image</h2>
+                    <input type="file" name="myImage" onChange={this.onImageChange} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Method1;
