7df780cd1d1934abce7f33431b58ece73a4894b3
Dhushyanth MK <dhushyanthmohan.kumar2019@vitstudent.ac.in>
2022-02-25 19:56:30 +0530

    Pre merging

---
+++ steganography-compression/src/python/method1.py
@@ -0,0 +1,10 @@
+import random
+
+def func():
+    print("Testing")
+    a = random.randint(1,10)
+    b = random.randint(1,10)
+    print("Random Sum:", a+b)
+
+if __name__ == "__main__":
+    func()
--- steganography-compression/src/templates/Method1.js
+++ steganography-compression/src/templates/Method1.js
@@ -1,38 +1,56 @@
-import { React, Component } from "react";
+import { React, Component, useState, useCallback, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 
-class Method1 extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            image: null
-        };
-    
-        this.onImageChange = this.onImageChange.bind(this);
-    }
+import script from '../python/method1.py';
+
+const runScript = async (code) => {
+    const pyodide = await window.loadPyodide({
+      indexURL : "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
+    });
   
-    onImageChange = event => {
+    return await pyodide.runPythonAsync(code);
+}
+
+const Method1 = () => {
+
+    const [output, setOutput] = useState("(loading...)");
+    const [image, setImageURL] = useState('');
+
+    const onImageChange = useCallback(event => {
         if (event.target.files && event.target.files[0]) {
-            let img = event.target.files[0];
-            this.setState({
-                image: URL.createObjectURL(img)
-            });
+            let pdf = event.target.files[0];
+            // this.setState({
+            //     image: URL.createObjectURL(img)
+            // });
+            setImageURL(URL.createObjectURL(pdf));
         }
-    };
-  
-    render() {
-        return (
+    }, []);
+
+    useEffect(() => {
+        const run = async () => {
+            const scriptText = await (await fetch(script)).text();
+            const out = await runScript(scriptText);
+            setOutput(out);
+        }
+        run();
+    }, []);
+    
+    return (
+        <div>
+            <h1>Method1</h1>
             <div>
-                <h1>Method1</h1>
                 <div>
-                    <div>
-                    <img src={this.state.image} />
-                    <h2>Select Image</h2>
-                    <input type="file" name="myImage" onChange={this.onImageChange} />
-                    </div>
+                <img src={image} />
+                <h2>Select Image</h2>
+                <input type="file" name="userDocument" onChange={onImageChange} />
+                </div>
+                <br />
+                <div className="" style={{border: '1px solid black'}}>
+                    {output}
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Method1;
