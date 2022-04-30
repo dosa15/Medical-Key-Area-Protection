3a8af36f609f9d3832b97fd8cf018d4ced4d7756
Dhushyanth MK <dhushyanthmohan.kumar2019@vitstudent.ac.in>
2022-04-30 13:54:02 +0530

    Improvements to the medical steganography algorithm

--- steganography-compression/medical_stego_decrypt.py
+++ steganography-compression/medical_stego_decrypt.py
@@ -5,18 +5,35 @@ import numpy as np
 from PIL import Image
 from rsa import encrypt
 
-testimage = Image.open("chest_mri_stego.png")
-testimage_array = np.asarray(testimage)
+def medical_stego_decrypt(input_image_file):
+    testimage = Image.open(input_image_file)
+    testimage_array = np.asarray(testimage)
 
-diagnostic = open("diagnostic_decrypted.txt", "w")
+    diagnostic = open("diagnostic_decrypted.txt", "w")
 
-key = np.load('key.npy')
-diagnostic_text = ''
+    key = list(np.load('key.npy', allow_pickle=True))
+    mode_pixel = key.pop()
+    print(key)
+    diagnostic_text = ''
 
-for [i, j] in key:
-    diagnostic_text += chr(testimage_array[i, j][0])
-    testimage_array[i, j] = np.array([0, testimage_array[i, j][1], testimage_array[i, j][2]])
+    for x in range(3):
+        for [i, j] in key[x]:
+            diagnostic_text += chr(testimage_array[i, j][x])
+            # if x == 0:
+            #     testimage_array[i, j] = np.array([mode_pixel, testimage_array[i, j][1], testimage_array[i, j][2]])
+            # elif x == 1:
+            #     testimage_array[i, j] = np.array([testimage_array[i, j][0], mode_pixel, testimage_array[i, j][2]])
+            # elif x == 2:
+            #     testimage_array[i, j] = np.array([testimage_array[i, j][0], testimage_array[i, j][1], mode_pixel])
+            testimage_array[i, j][x] = mode_pixel
 
-diagnostic.write(diagnostic_text)
-output = Image.fromarray(testimage_array)
-output.show()
+    diagnostic.write(diagnostic_text)
+    print("The diagnostic report was recovered into ", diagnostic.name)
+    output = Image.fromarray(testimage_array)
+    output.show()
+    output_image_file = input_image_file.split('_stego.')[0] + "_decrypted.jpeg"
+    output.save(output_image_file)
+
+if __name__ == "__main__":
+    input_image_file = "chest_mri_stego.png"
+    medical_stego_decrypt(input_image_file)
--- steganography-compression/medical_stego_encrypt.py
+++ steganography-compression/medical_stego_encrypt.py
@@ -5,29 +5,69 @@ import numpy as np
 from PIL import Image
 from rsa import encrypt
 
-testimage = Image.open("chest_mri.jpeg")
-testimage_array = np.asarray(testimage)
+def medical_stego_encrypt(input_image_file):
+    testimage = Image.open(input_image_file)
+    testimage_array = np.asarray(testimage)
 
-diagnostic = open("diagnostic.txt", "r")
-diagnostic_text = diagnostic.read()
-diagnostic_text = list(diagnostic_text)[-1::-1]
+    arr = testimage_array.flatten(order='C')
+    # with np.printoptions(threshold=np.inf):
+    #     print(np.sort(arr))
+    mode_pixel = np.bincount(testimage_array.flatten(order='C')).argmax()
+    print("Most common pixel value = ", mode_pixel, " occurring ", np.bincount(testimage_array.flatten(order='C'))[mode_pixel], " times")
 
-key = list()
-flag = False
-for i in range(testimage_array.shape[1]):
-    for j in range(testimage_array.shape[0]):
-        if testimage_array[i, j][0] < 1:
-            key.append([i,j])
-            x = ord(diagnostic_text.pop())
-            print(chr(x), end='')
-            testimage_array[i, j][0] = x
+    diagnostic = open("diagnostic.txt", "r")
+    diagnostic_text = diagnostic.read()
+    diagnostic_text = list(diagnostic_text)[-1::-1]
+
+    key0 = list()
+    for i in range(testimage_array.shape[1]):
+        for j in range(testimage_array.shape[0]):
+            if testimage_array[i, j][0] == mode_pixel:
+                key0.append([i,j])
+                x = ord(diagnostic_text.pop())
+                # print(chr(x), end='')
+                testimage_array[i, j][0] = x
+            if len(diagnostic_text) < 1:
+                break
         if len(diagnostic_text) < 1:
             break
-    if len(diagnostic_text) < 1:
-        break
-np.save('key.npy', key)
-            
+    
+    key1 = list()
+    if len(diagnostic_text) > 0:
+        for i in range(testimage_array.shape[1]):
+            for j in range(testimage_array.shape[0]):
+                if testimage_array[i, j][1] == mode_pixel:
+                    key.append([i,j])
+                    x = ord(diagnostic_text.pop())
+                    # print(chr(x), end='')
+                    testimage_array[i, j][1] = x
+                if len(diagnostic_text) < 1:
+                    break
+            if len(diagnostic_text) < 1:
+                break
+    
+    key2 = list()
+    if len(diagnostic_text) > 0:
+        for i in range(testimage_array.shape[1]):
+            for j in range(testimage_array.shape[0]):
+                if testimage_array[i, j][2] == mode_pixel:
+                    key.append([i,j])
+                    x = ord(diagnostic_text.pop())
+                    # print(chr(x), end='')
+                    testimage_array[i, j][2] = x
+                if len(diagnostic_text) < 1:
+                    break
+            if len(diagnostic_text) < 1:
+                break
 
-output = Image.fromarray(testimage_array)
-output.show()
-output.save("chest_mri_stego.png")
+    key = np.array([key0, key1, key2, mode_pixel], dtype=object)
+    np.save('key.npy', key)
+
+    output = Image.fromarray(testimage_array)
+    output.show()
+    output_image_file = input_image_file.split('.')[0] + "_stego.png"
+    output.save(output_image_file)
+
+if __name__ == '__main__':
+    input_image_file = "chest_mri.jpeg"
+    medical_stego_encrypt(input_image_file)
