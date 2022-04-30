7ce6c0c180a517f4930f3a55e9812393ae26cc09
Dhushyanth MK <dhushyanthmohan.kumar2019@vitstudent.ac.in>
2022-04-30 17:07:01 +0530

    Updates to pixel manipulation

--- steganography-compression/src/python/pixelmanipulation.py
+++ steganography-compression/src/python/pixelmanipulation.py
@@ -1,4 +1,4 @@
-from msilib.schema import File
+# from msilib.schema import File
 from functools import reduce
 import numpy as np
 
@@ -11,28 +11,32 @@ testimage.show()
 testimage_array= np.asarray(testimage)
 width,height=testimage.size
 
-for x in range(0,width,32):
-    for y in range(0,height,32):
-        #r,g,b=testimage.getpixel((x,y))
-        #r1,b1,g1 = testimage.getpixel((x+1,y+1))
-        #rt,bt,gt = r,g,b
-        #r,g,b = testimage.putpixel((x,y),(r1,b1,g1))
-        #r1,g1,b1 =testimage.putpixel((x+1,y+1),(rt,bt,gt))
+frame_size = 31
+for x in range(0,width,frame_size+1):
+    for y in range(0,height,frame_size+1):
+        # r,g,b=testimage.getpixel((x,y))
+        # r1,b1,g1 = testimage.getpixel((x+1,y+1))
+        # rt,bt,gt = r,g,b
+        # r,g,b = testimage.putpixel((x,y),(r1,b1,g1))
+        # r1,g1,b1 =testimage.putpixel((x+1,y+1),(rt,bt,gt))
     
-        for i in range(0,32):
-            for j in range(0,16):
+        for i in range(0,frame_size-1):
+            for j in range(0,frame_size-1):
                 try:
-                    p1= testimage_array[x+i,y+j]
-                    p2= testimage_array[abs((x+i)-(x+31))+x,abs((y+j)-(y+31))+y]
+                    if (width-x <= frame_size) or (height-y <= frame_size):
+                        break
+                    p1 = testimage_array[x+i, y+j]
+                    p2 = testimage_array[abs(i-frame_size)+x, abs(j-frame_size)+y]
 
-                    testimage_array[abs((x+i)-(x+31))+x,abs((y+j)-(y+31))+y]=p1
-                    testimage_array[x+i,y+j] = p2
+                    testimage_array[abs(i-frame_size)+x, abs(j-frame_size)+y] = p1
+                    testimage_array[x+i, y+j] = p2
                 except:
-                    
-                    print()
+                    print(x, y, i, j)
         
-testimage= Image.fromarray(testimage_array)
-testimage.save('src\python\distored.jpg')
+testimage = Image.fromarray(testimage_array)
+# testimage.save('src\python\distorted.jpeg')
+testimage.save('distorted.jpeg')
+
 testimage.show()
 
 
