200ccab8ccf3fb361284a2504932651f0a5b1d6b
Dhushyanth MK <dhushyanthmohan.kumar2019@vitstudent.ac.in>
2022-05-01 15:52:33 +0530

    First method for medical image steganography completed

--- steganography-compression/medical_stego_decrypt.py
+++ steganography-compression/medical_stego_decrypt.py
@@ -1,5 +1,7 @@
 import random
+from string import hexdigits
 import sys
+from turtle import width
 
 import numpy as np
 from PIL import Image
@@ -8,23 +10,31 @@ from rsa import encrypt
 def medical_stego_decrypt(input_image_file):
     testimage = Image.open(input_image_file)
     testimage_array = np.asarray(testimage)
+    width = height = 0
 
     diagnostic = open("diagnostic_decrypted.txt", "w")
 
     key = list(np.load('key.npy', allow_pickle=True))
     mode_pixel = key.pop()
-    print(key)
+    # print(key)
     diagnostic_text = ''
+
+    frame_size = 31
+    for x in range(0,width,frame_size):
+        for y in range(0,height,frame_size):
+            for i in range(0,frame_size-1):
+                for j in range(0,frame_size-1):
+                    if (width-x <= frame_size) or (height-y <= frame_size):
+                        break
+                    p1 = testimage_array[x+i, y+j]
+                    p2 = testimage_array[abs(i-frame_size)+x, abs(j-frame_size)+y]
+
+                    testimage_array[abs(i-frame_size)+x, abs(j-frame_size)+y] = p1
+                    testimage_array[x+i, y+j] = p2
 
     for x in range(3):
         for [i, j] in key[x]:
             diagnostic_text += chr(testimage_array[i, j][x])
-            # if x == 0:
-            #     testimage_array[i, j] = np.array([mode_pixel, testimage_array[i, j][1], testimage_array[i, j][2]])
-            # elif x == 1:
-            #     testimage_array[i, j] = np.array([testimage_array[i, j][0], mode_pixel, testimage_array[i, j][2]])
-            # elif x == 2:
-            #     testimage_array[i, j] = np.array([testimage_array[i, j][0], testimage_array[i, j][1], mode_pixel])
             testimage_array[i, j][x] = mode_pixel
 
     diagnostic.write(diagnostic_text)
